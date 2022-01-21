9234619d27eee1fecae4b1514fa0a89c567e085b
george <george.qian@fast-jet.com>
2022-01-21 16:38:12 +0800

    complete IndexView

--- MyToDo/ViewModels/IndexViewModel.cs
+++ MyToDo/ViewModels/IndexViewModel.cs
@@ -1,12 +1,66 @@
-﻿using System;
+﻿using MyToDo.Common.Models;
+using Prism.Mvvm;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MyToDo.ViewModels
 {
-    internal class IndexViewModel
+    public class IndexViewModel:BindableBase
     {
+        public IndexViewModel()
+        {
+            TaskBars = new ObservableCollection<TaskBar>();
+            CreateTaskBars();
+            CreateTestData();
+        }
+
+        private ObservableCollection<TaskBar> taskBars;
+
+        public ObservableCollection<TaskBar> TaskBars
+        {
+            get { return taskBars; }
+            set { taskBars = value; RaisePropertyChanged(); }
+        }
+
+        private ObservableCollection<ToDoDto> toDoDtos;
+
+        public ObservableCollection<ToDoDto> ToDoDtos
+        {
+            get { return toDoDtos; }
+            set { toDoDtos = value; RaisePropertyChanged(); }
+        }
+
+        private ObservableCollection<MemoDto> memoDtos;
+
+        public ObservableCollection<MemoDto> MemoDtos
+        {
+            get { return memoDtos; }
+            set { memoDtos = value; RaisePropertyChanged(); }
+        }
+
+        void CreateTaskBars()
+        {
+            taskBars.Add(new TaskBar() { Icon = "ClockFast", Title = "汇总", Content = "9", Color = "#FF0CA0FF", Target = "" });
+            taskBars.Add(new TaskBar() { Icon = "ClockCheckOutline", Title = "已完成", Content = "9", Color = "#FF1ECA3A", Target = "" });
+            taskBars.Add(new TaskBar() { Icon = "ChartLineVariant", Title = "完成率", Content = "100%", Color = "#FF02C6DC", Target = "" });
+            taskBars.Add(new TaskBar() { Icon = "PlaylistStar", Title = "备忘录", Content = "19", Color = "#FFFFA000", Target = "" });
+        }
+
+        void CreateTestData()
+        {
+            ToDoDtos = new ObservableCollection<ToDoDto>();
+            MemoDtos = new ObservableCollection<MemoDto>();
+
+            for(int i = 0; i < 10; i++)
+            {
+                ToDoDtos.Add(new ToDoDto() { Id = i, Title ="待办"+i,Content="正在处理..."});
+                MemoDtos.Add(new MemoDto() { Id = i, Title="备忘"+i,Content="我的密码"});
+            }
+        }
+
     }
 }
